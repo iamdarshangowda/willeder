d3c06a8abea45393f42ed6e77f06fec466a09d3a
Darshan Gowda <iamdarshangowda@gmail.com>
2023-11-14 08:51:06 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+{
+  /* <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */
+}
+
+function App() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
