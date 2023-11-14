b78d697cee2f670639ee0fd6bbc779fec19020b7
Darshan Gowda <iamdarshangowda@gmail.com>
2023-11-14 13:15:32 +0530

    menu open close function added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../Styles/Navbar.scss';
 
 const Navbar = () => {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const handleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo-container">
         <img src="./Union.png" alt="logo" />
         <h1>Logo</h1>
       </div>
-      <ul>
+      <ul className={menuActive ? 'menu-bar-open' : 'menu-bar-close'}>
         <li>Home</li>
         <li>Page 1</li>
         <li>Page 2</li>
       </ul>
-      <div className="menu-bar">
-        <img src="./menu.png" about="menu bar" />
+      <div className="menu-bar" onClick={handleMenu}>
+        {menuActive ? (
+          <img src="./close.webp" about="menu close" />
+        ) : (
+          <img src="./menu.png" about="menu bar" />
+        )}
       </div>
     </nav>
   );
