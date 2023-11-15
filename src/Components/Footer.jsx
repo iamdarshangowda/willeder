1091ad506d99f1f3b7843a7154260391dc6a62aa
Darshan Gowda <iamdarshangowda@gmail.com>
2023-11-15 19:30:32 +0530

    footer added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import '../Styles/Footer.scss';
+import facebookLogo from './Icons/facebook.svg';
+import instagramLogo from './Icons/instagram.svg';
+import twitterLogo from './Icons/twitter.svg';
+import tiktokLogo from './Icons/tiktok.svg';
 
 const Footer = () => {
-  return <div style={{ height: '300px', background: '#f2f2f2' }}>Footer</div>;
+  return (
+    <div className="footer-container">
+      <ul>
+        <li>Home</li>
+        <li>Page 1</li>
+        <li>Page 2</li>
+      </ul>
+      <hr />
+      <div className="social-container">
+        <img src={facebookLogo} alt="facebookLogo" className="social_icon" />
+        <img src={instagramLogo} alt="instagramLogo" className="social_icon" />
+        <img src={twitterLogo} alt="twitterLogo" className="social_icon" />
+        <img src={tiktokLogo} alt="tiktokLogo" className="social_icon" />
+      </div>
+      <p>© Logo, Inc.</p>
+    </div>
+  );
 };
 
 export default Footer;
