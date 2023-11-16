9bc22b99fb57427ca5538de93f2ad8b932e39c3e
Darshan Gowda <iamdarshangowda@gmail.com>
2023-11-16 18:28:59 +0530

    shade added for triplet images

--- src/Components/VerticalPage/TripletImages.jsx
+++ src/Components/VerticalPage/TripletImages.jsx
@@ -9,9 +9,10 @@ const TripletImages = () => {
         <p className="bar-subTitle">subtitle</p>
       </div>
       <div className="background-card">
+        <div className="shade"></div>
         <img src="./background-1.png" alt="background-1" />
         <div className="image-text">
-          <p className="header-text">01</p>
+          <p className="header-number">01</p>
           <p className="header-text">Lorem ipsum dolor sit</p>
           <p className="sub-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit
@@ -19,9 +20,10 @@ const TripletImages = () => {
         </div>
       </div>
       <div className="background-card">
+        <div className="shade"></div>
         <img src="./background-2.png" alt="background-2" />{' '}
         <div className="image-text">
-          <p className="header-text">02</p>
+          <p className="header-number">02</p>
           <p className="header-text">Lorem ipsum dolor sit</p>
           <p className="sub-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit
@@ -29,9 +31,10 @@ const TripletImages = () => {
         </div>
       </div>
       <div className="background-card">
+        <div className="shade"></div>
         <img src="./background-3.png" alt="background-3" />
         <div className="image-text">
-          <p className="header-text">03</p>
+          <p className="header-number">03</p>
           <p className="header-text">Lorem ipsum dolor sit</p>
           <p className="sub-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit
