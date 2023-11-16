c7015b10fb677b97368e7a9b573596ac9d5311f4
Darshan Gowda <iamdarshangowda@gmail.com>
2023-11-16 19:52:22 +0530

    UI updates

--- src/Components/Description.jsx
+++ src/Components/Description.jsx
@@ -8,13 +8,17 @@ const Description = () => {
         <img src="./Union.png" alt="logo" />
         <h1>Logo</h1>
       </div>
-      <p className="bar-text">Lorem ipsum dolor sit amet</p>
-      <p className="text-description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua.
-      </p>
-      <p className="scroll-text">scroll</p>
-      <div className="line"></div>
+      <div className="bar-container">
+        <p className="bar-text">Lorem ipsum dolor sit amet</p>
+        <p className="text-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div className="scroll-container">
+        <p className="scroll-text">scroll</p>
+        <div className="line"></div>
+      </div>
     </div>
   );
 };
